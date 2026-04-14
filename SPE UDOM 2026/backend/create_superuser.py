7d8af92a70ffb86d0d824eb87c1493cecf2ac951
mathias-hash <ettiferh119mathias@gmail.com>
2026-04-15 00:52:56 +0300

    update changes

--- SPE UDOM 2026/backend/create_superuser.py
+++ SPE UDOM 2026/backend/create_superuser.py
@@ -12,7 +12,9 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-ADMIN_EMAIL = 'admin@speudom.ac.tz'
+ADMIN_EMAIL = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'speudomadmin@gmail.com')
+ADMIN_NAME = os.environ.get('DJANGO_SUPERUSER_NAME', 'SPE Administrator')
+ADMIN_LOGIN_URL = os.environ.get('ADMIN_LOGIN_URL', 'http://localhost:8000/admin')
 
 if User.objects.filter(email=ADMIN_EMAIL).exists():
     print(f'Superuser already exists: {ADMIN_EMAIL}')
@@ -22,7 +24,7 @@ password = os.environ.get('DJANGO_SUPERUSER_PASSWORD') or secrets.token_urlsafe(
 
 user = User.objects.create_superuser(
     email=ADMIN_EMAIL,
-    full_name='SPE Administrator',
+    full_name=ADMIN_NAME,
     password=password,
 )
 
@@ -34,4 +36,4 @@ if 'DJANGO_SUPERUSER_PASSWORD' in os.environ:
     print('Password: loaded from DJANGO_SUPERUSER_PASSWORD')
 else:
     print(f'Generated password: {password}')
-print('\nLogin at: http://localhost:8000/admin')
+print(f'\nLogin at: {ADMIN_LOGIN_URL}')
